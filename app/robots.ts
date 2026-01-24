89ec1ff4d16f452acbdf65fab9844acb31d0c50a
Richardson <richardson@Richardsons-MacBook-Air.local>
2026-01-25 01:42:35 +0530

    fix(build): Correct robots.txt syntax rule array

--- app/robots.ts
+++ app/robots.ts
@@ -2,15 +2,17 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: ['/dashboard/', '/api/'],
-        },
-        {
-        userAgent: ['Google-Extended', 'GPTBot', 'CCBot'],
-            allow: '/',
-        },
-    sitemap: 'https://rihario.com/sitemap.xml',
+        rules: [
+            {
+                userAgent: '*',
+                allow: '/',
+                disallow: ['/dashboard/', '/api/'],
+            },
+            {
+                userAgent: ['Google-Extended', 'GPTBot', 'CCBot'],
+                allow: '/',
+            }
+        ],
+        sitemap: 'https://rihario.com/sitemap.xml',
     }
 }
